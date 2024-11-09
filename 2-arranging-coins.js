e1427bd60bbd657511cbfe1f1b2519e5c6a7a42b
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-11-09 09:54:12 -0500

    add changes

--- 2-arranging-coins.js
+++ 2-arranging-coins.js
@@ -1,10 +1,12 @@
 const arrangingCoins = (n) => {
-  for (let i = 1; i <= n; i++) {
-    n -= i;
-    if (n < i + 1) {
-      return i;
-    }
-  }
+  // for (let i = 1; i <= n; i++) {
+  //   n -= i;
+  //   if (n < i + 1) {
+  //     return i;
+  //   }
+  // }
+
+  return Math.floor((-1 + Math.sqrt(1 + 8 * n) / 2))
 }
 
-arrangingCoins(5) // 2
+console.log(arrangingCoins(8)) // 2
