5d16314b5d97ef57ba7045a8eeafc80f1d62fce3
Edu <106676335+EdwardMelendezM@users.noreply.github.com>
2025-01-18 21:58:16 -0500

    Create 21. longestPalindrome.go

---
+++ 21. longestPalindrome.go
@@ -0,0 +1,29 @@
+func longestPalindrome(s string) string {
+    if len(s) == 0 {
+        return ""
+    }
+    start, end := 0, 0
+
+    expandAroundCenter := func(left, right int)(int, int) {
+        for (left >= 0 && right < len(s) && s[left]==s[right]){
+            left--
+            right++
+        }
+        return left + 1, right - 1
+    }
+
+    for i:=0; i < len(s); i++ {
+        l1, r1 := expandAroundCenter(i,i)
+        l2, r2 := expandAroundCenter(i, i+1)
+
+        if r1-l1 > end-start {
+            start, end = l1, r1
+        }
+
+        if r2-l2 > end-start {
+            start, end = l2, r2
+        }
+    }
+
+    return s[start:end+1]
+}
