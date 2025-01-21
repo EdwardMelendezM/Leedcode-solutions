66bb546ba52e5bbb9c333c11b4040ea9f4c23658
Edu <106676335+EdwardMelendezM@users.noreply.github.com>
2025-01-21 11:16:18 -0500

    Create 32.isValid.cpp

---
+++ 32.isValid.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        unordered_map<char,char> mapping = {
+            {')','('},
+            {'}','{'},
+            {']','['}
+        };
+
+        for (char c: s) {
+            if (mapping.count(c)){
+                char topElement = stack.empty() ? '#' : stack.top();
+                if ( topElement == mapping[c] ) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return true;
+    }
+};
